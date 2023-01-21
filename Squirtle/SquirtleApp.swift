a4aaa24090df9831bf5ae8ec2ad1bc7958c6b665
Łukasz Galiński <galczo230@gmail.com>
2023-01-21 19:27:42 +0100

    SQUIR_01: Initial push

---
+++ Squirtle/SquirtleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SquirtleApp.swift
+//  Squirtle
+//
+//  Created by Łukasz Galiński on 21/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct SquirtleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
